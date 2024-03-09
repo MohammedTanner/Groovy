d49f4bd46124b0192ad73f08eb23184cbc3d5d93
Mohammed Tanner <albohasabullah.m@northeastern.edu>
2024-03-09 01:28:36 -0500

    added login and update role

--- Auth/Auth.js
+++ Auth/Auth.js
@@ -22,3 +22,68 @@ exports.register = async (req, res, next) => {
         })
     }
 }
+
+exports.login = async (req, res, next) => {
+    const { username, password } = req.body
+    if (!username || !password) {
+        return res.status(400).json({
+            message: "Username or Password not present",
+        })
+    }
+    try {
+        const user = await User.findOne({ username, password })
+        if (!user) {
+            res.status(401).json({
+                message: "Login not successful",
+                error: "User not found", 
+            })
+        } else {
+            res.status(200).json({
+                message: "Login successful",
+                user,
+            })
+        }
+    } catch (error) {
+        res.status(400).json({
+            message: "An error occured",
+            error: error.message,
+        })
+    }
+}
+
+exports.update = async (req, res, next) => {
+    const { role, id } = req.body
+    // First - Verifying if role and id is present
+    if (role && id) {
+        // Second - Verifying if the value of role is admin
+        if (role === "admin") {
+            // Finds the user with the id
+            await User.findById(id)
+            .then((user) => {
+                // Third - Verifies the user is not an admin
+                if (user.role !== "admin") {
+                    user.role = role;
+                    user.save((err) =>{
+                        // Mongodb error checker
+                        if (err) {
+                            res.status(400).json({ message: "An error occurred", error: err.message });
+                            process.exit(1);
+                        }
+                        res.status(201).json({ message: "Update successful", user });
+                    });
+                } else {
+                    res.status(400).json({ message: "User is already an Admin" });
+                }
+            })
+            .catch((error) => {
+                res.status(400).json({ message: "An error occurred", error: error.message });
+            })
+        } else {
+            res.status(400).json({
+                message: "Role is not admin",
+            })
+        }
+    } else {
+        res.status(400).json({ message: "Role or ID not present" })
+    }
+}
--- Auth/Route.js
+++ Auth/Route.js
@@ -1,5 +1,7 @@
-const express = require("express")
-const router = express.Router()
-const { register } = require("./Auth")
-router.route("/register").post(register)
-module.exports = router
+const express = require("express");
+const router = express.Router();
+const { register, login, update } = require("./Auth");
+router.route("/register").post(register);
+router.route("/login").post(login);
+router.route("/update").put(update); // We use put() instead of post like with login and register. put is restricted to create or update operations while POST is not limited
+module.exports = router;
