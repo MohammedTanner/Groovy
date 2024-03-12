1e1b07fe0949700a1794c9eda00384949a626f49
Mohammed Tanner <albohasabullah.m@northeastern.edu>
2024-03-12 05:45:51 -0400

    Login functionality changes and bug fixes

--- Auth/Auth.js
+++ Auth/Auth.js
@@ -114,3 +114,19 @@ exports.deleteUser = async (req, res, next) => {
         .catch(error => res.status(400).json({ message: "An error occurred", error: error.message })
         )
 }
+
+exports.getUsers = async (req, res, next) => {
+    await User.find({})
+    .then(users => {
+        const userFunction = users.map(user => {
+            const container = {}
+            container.username = user.username
+            container.role = user.role
+            return container
+        })
+        res.status(200).json({ user: userFunction })
+    })
+    .catch(err =>
+        res.status(401).json({ message: "Not successful", error: err.message })
+        )
+}
